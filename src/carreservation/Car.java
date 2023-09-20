d4f8f442dab1d81f45b30d10ae1419adb47b4be3
therm11011 <giaaanmari@gmail.com>
2023-09-20 14:12:21 +0800

    message

---
+++ src/carreservation/Car.java
@@ -0,0 +1,21 @@
+package carreservation;
+
+public class Car {
+	String brand;
+	String color;
+	
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MovieReservation {
+}
